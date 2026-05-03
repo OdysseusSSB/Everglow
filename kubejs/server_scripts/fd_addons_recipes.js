62261334879fecfd499a090bbc3ee74d1dbb80a6
OdysseusSSB <jakedotsed@gmail.com>
2026-05-03 12:10:51 +1000

    Configuration of H&H and Vintage Delight

---
+++ kubejs/server_scripts/fd_addons_recipes.js
@@ -0,0 +1,39 @@
+ServerEvents.recipes(event => {
+  event.shaped(
+    Item.of('hearthandharvest:cotton_candy'),
+    [
+      ' S ',
+      'SCS',
+      ' P '
+    ],
+    {
+      C: 'vintage_delight:cotton',
+      P: 'minecraft:stick',
+      S: 'minecraft:sugar'
+    }
+  );
+  event.shaped(
+    Item.of('hearthandharvest:cotton_bale'),
+    [
+      'CCC',
+      'CCC',
+      'CCC'
+    ],
+    {
+      C: 'vintage_delight:cotton'
+    }
+  );
+  event.replaceInput(
+    //{ input: 'hearthandharvest:peanut' },
+    'hearthandharvest:peanut',
+    'vintagedelight:peanut'
+  );
+  event.replaceInput(
+    'hearthandharvest:roasted_peanuts',
+    'vintagedelight:roasted_peanut'
+  );
+  event.replaceOutput(
+    'hearthandharvest:cotton',
+    'rustic_delight:cotton_boll'
+  );
+})
